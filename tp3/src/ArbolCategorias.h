7576cf079ec51d3fe6377ab57fb72a70392314e7
abarabas <knoid@users.noreply.github.com>
2012-11-05 14:40:59 +0000

    Ahora está bien hecha la declaracion de friend en los iteradores (gracias a Brian)

--- tp3/src/ArbolCategorias.h
+++ tp3/src/ArbolCategorias.h
@@ -26,11 +26,23 @@ class ArbolCategorias {
 
 public:
 
+	class IteradorHijas;
+	class IteradorPadres;
+
+	ArbolCategorias(const Categoria& raiz);
+	virtual ~ArbolCategorias();
+
+	const Categoria raiz() const;
+	void agregar(const Categoria& padre, const Categoria& hija);
+	IteradorHijas hijas(const Categoria& padre) const;
+	IteradorPadres padres(const Categoria& hija) const;
+	Nat id(const Categoria& c) const;
+	Nat cantCategorias() const;
+
 	class IteradorHijas {
 		Lista<Categoria>::Iterador _it;
 		IteradorHijas(Lista<Categoria>* lista);
-		friend class ArbolCategorias;
-//		friend class ArbolCategorias::IteradorHijas ArbolCategorias::hijas(const Categoria& padre) const;
+		friend class ArbolCategorias::IteradorHijas ArbolCategorias::hijas(const Categoria& padre) const;
 
 	public:
 		void Avanzar();
@@ -42,23 +54,13 @@ public:
 		CatInfo* _catInfo;
 
 		IteradorPadres(CatInfo* catInfo);
-		friend class ArbolCategorias;
+		friend class ArbolCategorias::IteradorPadres ArbolCategorias::padres(const Categoria& padre) const;
 
 	public:
 		void Avanzar();
 		bool HaySiguiente() const;
 		Nat Siguiente() const;
 	};
-
-	ArbolCategorias(const Categoria& raiz);
-	virtual ~ArbolCategorias();
-
-	const Categoria raiz() const;
-	void agregar(const Categoria& padre, const Categoria& hija);
-	IteradorHijas hijas(const Categoria& padre) const;
-	IteradorPadres padres(const Categoria& hija) const;
-	Nat id(const Categoria& c) const;
-	Nat cantCategorias() const;
 };
 
 #endif /* ARBOLCATEGORIAS_H_ */

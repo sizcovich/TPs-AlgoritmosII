02798250ebd3db7dcf2350e9632246b82a54c66c
sebastianvita <sevita@users.noreply.github.com>
2012-11-19 18:46:30 +0000

    puede ser q este commit rompa todo pero lo tengo q llevar a la facu xD

--- tp3/src/LinkLinkIt.cpp
+++ tp3/src/LinkLinkIt.cpp
@@ -1,4 +1,7 @@
 #include "LinkLinkIt.h"
+#include "aed2/Lista.h"
+#include "aed2/TiposBasicos.h"
+
 
 LinkLinkIt::LinkLinkIt(ArbolCategorias& abCat) : _aCategorias(abCat) {
 	_infoLinks = new DiccString<InfoLink>();
@@ -58,8 +61,66 @@ void LinkLinkIt::accederLink(const Link& l, const Fecha& f) {
 
 /*
 LinkLinkIt::IteradorLinks LinkLinkIt::linksOrdenadosPorAccesos(const Categoria& c){
+    int categoriaID = _aCategorias.id(c);
+    Lista<InfoLink*> lista = _linksPorCat[categoriaID - 1];
 
+    const_Iterador it = lista.CrearIt();
+    int f = 0;
 
+    while(it.HaySiguiente()) {
+        f = max(it.Siguiente.ultAcceso, f);
+        it.Avanza();
+    }
+
+    const_Iterador it = lista.CrearIt();
+    bool estaOrdenada = true;
+    int ultPuntaje = -1;
+    while(it.HaySiguiente() && estaOrdenada) {
+        if(ultPuntaje > -1)
+            estaOrdenada = ultPuntaje >= puntajeDelLink(it.Siguiente(), f);
+        ultPuntaje = puntajeDelLink(it.Siguiente(), f);
+    }
+    if(estaOrdenada)
+        return IteradorLinks(lista, f);
+
+    Lista listaOrdenada = Lista();
+    const_Iterador itOrd = listaOrdenada.CrearIt();
+    while(!lista.esVacia()) {
+        const_Iterador itRes, itMaxAccesos = lista.CrearIt();
+        while(itRes.HaySiguiente())
+            if (puntajeDelLink(itRes.Siguiente(), f) > puntajeDelLink(itMaxAccesos.Siguiente, f))
+                itMaxAccesos = itRes;
+        itOrd.AgregarComoAnterior(itMaxAccesos.Siguiente())
+        itMaxAccesos.EliminarSiguiente();
+
+    }
+    _linksPorCat[categoriaID - 1] = listaOrdenada;
+    return IteradorLinks(listaOrdenada, f);
 }*/
 
+LinkLinkIt::IteradorLinks LinkLinkIt::IteradorLinks::IteradorLinks(Lista<InfoLink> ls, Fecha f) {
+    _it = ls.Iterador();
+    _ultAcceso = f;
+}
 
+Link LinkLinkIt::IteradorLinks::SiguienteLink(IteradorLinks itl) {
+    return itl._it.Siguiente().link;
+}
+
+Categoria LinkLinkIt::IteradorLinks::SiguienteCategoria(IteradorLinks itl) {
+    return itl._it.Siguiente().categoria;
+}
+
+///HAY Q ARREGLAR PUNTAJE DEL LINK, RECIVE 2 PARAMETROS Y SIEMPRE LE PASAN 3
+/*
+Nat LinkLinkIt::IteradorLinks::SiguienteAccesosRecientes(IteradorLinks itl) {
+    return puntajeDelLink();
+}
+
+bool LinkLinkIt::IteradorLinks::HaySiguiente(IteradorLinks itl) {
+    return itl._it.Siguiente().categoria;
+}*/
+
+ LinkLinkIt::IteradorLinks::Avanzar()(IteradorLinks itl) {
+    return itl._it.Avanzar();
+}
